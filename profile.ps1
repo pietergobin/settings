a15ccadacbdb57478d5755c1758c60be56752221
pietergobin <58220411+pietergobin@users.noreply.github.com>
2025-04-15 10:06:48 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -10,11 +10,23 @@ function Get-KubectlPods {
 }
 
 
+
+function Switch-Kubernetes-Namespaces {
+    param(
+        $n 
+    )
+    kubectl config set-context --current --namespace=$n
+}
+
+
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 Set-Alias grep Select-String
 Set-Alias pods Get-KubectlPods
 Set-Alias which Get-Command
+Set-Alias touch New-Item
 Set-Alias kc kubectl
+Set-Alias wc Measure-Object
+Set-Alias kswitch Switch-Kubernetes-Namespaces
 
 # Import-Module CompletionPredictor
 Set-PSReadLineOption -PredictionSource History
@@ -22,3 +34,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 
 
 oh-my-posh init pwsh | Invoke-Expression
+
